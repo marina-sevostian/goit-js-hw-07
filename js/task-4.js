17a155bb59f488a1a6823984b4a8585fb4b72d81
Marina.S. <marina.sevostian@gmail.com>
2024-10-07 00:44:21 +0300

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const registerForm = document.querySelector('form.login-form');
 
+registerForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === '' || password === '') {
+    return alert(`All form fields must be filled in`);
+  }
+  const userInfo = {
+    email: email.trim(),
+    Password: password.trim(),
+  };
+  console.log(userInfo);
+  form.reset();
+}
