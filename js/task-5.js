8c285d7781e18d704999c8db4f41e25043e6ef6a
Marina.S. <marina.sevostian@gmail.com>
2024-10-07 02:36:58 +0300

    add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColor = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+changeColor.addEventListener('click', handleClick);
+function handleClick(event) {
+  const form = event.target;
+  form.style.backgroundColor = getRandomHexColor();
+  colorSpan.innerHTML = form.style.backgroundColor;
+}
